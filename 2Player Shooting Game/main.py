0dc4af995b5f13b9fb16ff49800d8eb6696755dc
VincentEPython <vincent.emelone@zugerklassen.ch>
2026-06-03 16:00:03 +0200

    Add files via upload

---
+++ 2Player Shooting Game/main.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+#constants
+FPS = 60
+WIDTH = 1000
+HEIGHT = 600
+PLAYER_SPEED = 5
+BULLET_SPEED = 3
+MAX_BULLET = 3
+DIVIDER = pygame.Rect(WIDTH // 2 -5, 0,10,HEIGHT)
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+#adding title to window
+pygame.display.set_caption("Laser Royale")
+
+#setting the font
+hp_font = pygame.font.SysFont("courier",20)
+win_lose_font = ""
+while True:
+    screen.fill("navy blue")
+
+    #displaying the Divider
+    pygame.draw.rect(screen,"white",DIVIDER)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+    pygame.display.update()
