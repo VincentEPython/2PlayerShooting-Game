e1479af7860e12167795bc6ae6f93232fa7ce1d0
VincentEPython <vincent.emelone@zugerklassen.ch>
2026-06-10 15:59:20 +0200

    Update main.py

--- 2Player Shooting Game/main.py
+++ 2Player Shooting Game/main.py
@@ -16,14 +16,29 @@ pygame.display.set_caption("Laser Royale")
 
 #setting the font
 hp_font = pygame.font.SysFont("courier",20)
-win_lose_font = ""
-while True:
-    screen.fill("navy blue")
+win_lose_font = pygame.font.SysFont("Times New Roman",30)
 
+#creating Player class
+class Player(pygame.sprite.Sprite):
+    def __init__(self,x,y,img_path,controls,side):
+        super().__init__()
+        self.img = pygame.image.load(img_path)
+
+
+
+
+
+def draw():
+    screen.fill("navy blue")
     #displaying the Divider
     pygame.draw.rect(screen,"white",DIVIDER)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
-    pygame.display.update()
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
+        pygame.display.update()
+
+main()
